b92502fef631511def2803fe8c87e79377c298e1
aexizn <janessa.pandiyan@cit.edu>
2022-12-17 02:26:08 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,11 +1,33 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class LeapYearGUI {
+
+public class LeapYearGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());
+                if (year % 4 != 0) {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                } else if (year % 100 != 0) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else if (year % 400 != 0) {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                }
+            }
+        });
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Leap Year Checker");
